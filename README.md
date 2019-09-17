93fc38169fadb5f69df197a670714a4cfe57ab36
afeiship <1290657123@qq.com>
2019-09-17 17:05:09 +0800

    docs: test

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ npm install -S afeiship/next-strip-link --registry=https://registry.npm.taobao.o
 
 ## usage
 ```js
-//DOCS here!
+import 'next-strip-link';
+
+// url
+nx.stripLink('https://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5MzMwODY4NA==&appmsgid=10000043&itemidx=1&sign=9adf0474abe6c7fb20abd2c847bc7133#rd')
 ```
--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 var nx = require('next-js-core2');
 require('../src/next-strip-link');
+describe('api.test', function() {
+  test('nx.stripLink', function() {
+    var url1 = 'https://github.com/afeiship';
+    var url2 =
+      'https://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5MzMwODY4NA==&appmsgid=10000043&itemidx=1&sign=9adf0474abe6c7fb20abd2c847bc7133#wechat_redirect';
 
+    var rs1 = nx.stripLink(url1);
+    var rs2 = nx.stripLink(url2);
 
-test('nx.stripLink', function () {
-  var obj1 = {name: 'fei'};
-  var obj2 = {email: '1290657123@qq.com'};
-
-  var result = {};
-
-  nx.stripLink(result, obj1, obj2);
-
-  expect(result.name, obj1.name).toBe(null);
+    expect(rs1).toBe('https://github.com/afeiship');
+    expect(rs2).toBe(
+      'https://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5MzMwODY4NA==&appmsgid=10000043&itemidx=1&sign=9adf0474abe6c7fb20abd2c847bc7133'
+    );
+  });
 });
-
