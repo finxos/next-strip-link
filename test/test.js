11b8a7a26e80d92b31599bb280c7d38195c7adec
afeiship <1290657123@qq.com>
2019-09-17 17:10:41 +0800

    add: null url

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var nx = require('next-js-core2');
 require('../src/next-strip-link');
 describe('api.test', function() {
-  test('nx.stripLink', function() {
+  test('nx.strip link null', function() {
+    var url = null;
+    var rs1 = nx.stripLink(url);
+    expect(rs1).toBe(null);
+  });
+
+  test('nx.stripLink normal', function() {
     var url1 = 'https://github.com/afeiship';
     var url2 =
       'https://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5MzMwODY4NA==&appmsgid=10000043&itemidx=1&sign=9adf0474abe6c7fb20abd2c847bc7133#wechat_redirect';
